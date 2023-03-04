d230e08e1447b92ba2e231232dd8c05f6315a0a1
Ali Raza <ialidar2001@gmail.com>
2023-03-04 14:41:02 +0500

    Changes in Login Screen code

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,15 +12,106 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 32),
         width: double.infinity,
         child: Column(
-          children: const [
-            SizedBox(
+          children: [
+            Flexible(flex: 2, child: Container()),
+            // Adding Image
+            Image.asset(''),
+            const SizedBox(
               height: 100,
             ),
-            TextFieldInput(),
-            SizedBox(
+            const TextFieldInput(),
+            const SizedBox(
               height: 20,
             ),
-            TextFieldInput(),
+            const TextFieldInput(),
+            InkWell(
+              child: Container(
+                padding: const EdgeInsets.only(left: 220, top: 10),
+                height: 100,
+                child: const Text('Forgot Password?'),
+              ),
+            ),
+            InkWell(
+              child: Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 10,
+                      offset: Offset(0, 5),
+                    )
+                  ],
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.amber,
+                ),
+                child: const Text(
+                  "LogIn",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            InkWell(
+              child: Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 10,
+                      offset: Offset(0, 5),
+                    )
+                  ],
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.white,
+                ),
+                child: const Text(
+                  "Sign In with Google",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            InkWell(
+              child: Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 10,
+                      offset: Offset(0, 5),
+                    )
+                  ],
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.blue,
+                ),
+                child: const Text(
+                  "Sign In with Facebook",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
