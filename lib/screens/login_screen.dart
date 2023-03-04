5bf080dc1b5613e37abd4b64979c901ad90b2dc9
Ali Raza <ialidar2001@gmail.com>
2023-03-04 16:54:23 +0500

    Added widgets to Login Screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -15,15 +15,29 @@ class LoginScreen extends StatelessWidget {
           children: [
             Flexible(flex: 2, child: Container()),
             // Adding Image
-            Image.asset(''),
-            const SizedBox(
-              height: 100,
+            Image.asset(
+              'assets/images/background_authScreen.png',
+              width: 378,
+              height: 213,
             ),
-            const TextFieldInput(),
+            Image.asset(
+              'assets/images/logo.png',
+            ),
+
             const SizedBox(
               height: 20,
             ),
+
             const TextFieldInput(),
+
+            const SizedBox(
+              height: 5,
+            ),
+
+            const TextFieldInput(),
+            SizedBox(
+              height: 20,
+            ),
             InkWell(
               child: Container(
                 padding: const EdgeInsets.only(left: 220, top: 10),
@@ -56,62 +70,60 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            InkWell(
-              child: Container(
-                width: double.infinity,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                decoration: const BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 10,
-                      offset: Offset(0, 5),
-                    )
-                  ],
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  color: Colors.white,
-                ),
-                child: const Text(
-                  "Sign In with Google",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            InkWell(
-              child: Container(
-                width: double.infinity,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                decoration: const BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 10,
-                      offset: Offset(0, 5),
-                    )
-                  ],
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  color: Colors.blue,
-                ),
-                child: const Text(
-                  "Sign In with Facebook",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-            ),
+
+            // InkWell(
+            //   child: Container(
+            //     width: double.infinity,
+            //     alignment: Alignment.center,
+            //     padding: const EdgeInsets.symmetric(vertical: 12),
+            //     decoration: const BoxDecoration(
+            //       boxShadow: [
+            //         BoxShadow(
+            //           color: Colors.grey,
+            //           blurRadius: 10,
+            //           offset: Offset(0, 5),
+            //         )
+            //       ],
+            //       borderRadius: BorderRadius.all(Radius.circular(10)),
+            //       color: Colors.white,
+            //     ),
+            //     child: const Text(
+            //       "Sign In with Google",
+            //       style: TextStyle(
+            //         fontSize: 20,
+            //         fontWeight: FontWeight.normal,
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            // const SizedBox(
+            //   height: 20,
+            // ),
+            // InkWell(
+            //   child: Container(
+            //     width: double.infinity,
+            //     alignment: Alignment.center,
+            //     padding: const EdgeInsets.symmetric(vertical: 12),
+            //     decoration: const BoxDecoration(
+            //       boxShadow: [
+            //         BoxShadow(
+            //           color: Colors.grey,
+            //           blurRadius: 10,
+            //           offset: Offset(0, 5),
+            //         )
+            //       ],
+            //       borderRadius: BorderRadius.all(Radius.circular(10)),
+            //       color: Colors.blue,
+            //     ),
+            //     child: const Text(
+            //       "Sign In with Facebook",
+            //       style: TextStyle(
+            //         fontSize: 20,
+            //         fontWeight: FontWeight.normal,
+            //       ),
+            //     ),
+            //   ),
+            // ),
           ],
         ),
       ),
